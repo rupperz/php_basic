f8001a1ed20d62b2b5ba0a1a6d2e677e7a67c610
rupperz <zubcu_victor@yahoo.com>
2021-10-21 15:43:11 +0300

    commit 10

--- day3/date/index.php
+++ day3/date/index.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+error_reporting(-1);
 date_default_timezone_set('Europe/Chisinau'); //set timezone
 
 //------------ Function date
-echo date('d-m-Y H:i:s');
+/* echo date('d-m-Y H:i:s');
 echo "<br>";
 echo date('d-m-Y h:i:s A'); // echo date  time in 12h format
 echo "<br>";
@@ -14,12 +15,12 @@ echo date('d-m-Y h:i:s', time() + 60 * 60 * 24 * 7 ); // adding 7 day time to da
 echo "<br>";
 echo time(); // Unix time from 01.01.1970
 echo "<br>";
-echo "2020 - " . date('Y'); // echo this year
+echo "2020 - " . date('Y'); // echo this year */
 
 //phpinfo(); //information about your server configuration
-echo "<br>";
+/* echo "<br>";
 $date = getdate();
-print_r($date);
+print_r($date); */
 /*
 Array
 (
@@ -36,20 +37,61 @@ Array
     [0]       => 1055901520
 )
 */
-echo "<br>";
+/* echo "<br>";
 echo date('d-m-Y h:i:s', strtotime("+1 day 1 hour")); // tomorrow date show
 echo "<br>";
-echo date('d-m-Y h:i:s', time() + 60 * 60 * 24);  // tomorow date
+echo date('d-m-Y h:i:s', time() + 60 * 60 * 24);  // tomorow date */
 
 
 // ------------- Function mktime
-echo "<br>";
+/* echo "<br>";
 echo time();
 echo "<br>";
 echo mktime( date("H") + 1, date("i") + 1, date("s") + 1, date("m"), date("d"), date("Y") );
 echo "<br>";
 $t = mktime( date("H"), date("i"), date("s"), date("m"), date("d"), date("Y") + 1 ); //adding 1 year
-echo date('Y-m-d H:i:s', $t);
+echo date('Y-m-d H:i:s', $t); */
+
+
+// -------- Function date_create
+$date = date_create(date('Y-m-d H:i:s'));
+//var_dump($date);
+echo date_format($date, 'Y-m-d H:i:s');
+
+
+// -------- Function date_add
+date_add($date, date_interval_create_from_date_string('2 days + 2 hours + 10 minutes + 5seconds'));
+echo "<br>";
+echo date_format($date, 'Y-m-d H:i:s');
+echo "<br>";
+
+// -------- Function microtime
+echo microtime(); //string
+echo "<br>";
+echo microtime(true); // boolean
+echo "<br>";
+
+
+//-   Execution time of page
+/* $start = microtime(true);
+usleep(1000000);
+$end = microtime(true);
+echo $end - $start; */
+
+//exemple 2 timer
+$start = microtime(true);
+for($i = 1; $i <= 1000000; $i++){
+    if($i == 1000000) echo "Отчет завершен за: ";
+}
+$end = microtime(true);
+
+
+echo $end - $start;
+
+// -------- Function date_create
+
+
+
 
 
 
